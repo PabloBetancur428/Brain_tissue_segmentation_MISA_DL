41021de1a0be2263b55ac2add420a0b24af15274
PabloBetancur28 <pablo1326699@gmail.com>
2024-11-29 22:22:12 +0100

    Intensity histogram of subjects

---
+++ src/subject_variability.py
@@ -0,0 +1,24 @@
+from overlay import load_nifti_file
+import matplotlib.pyplot as plt
+
+def compary_intensity_histograms(image_paths):
+
+    for path in image_paths:
+        image = load_nifti_file(path)
+        image = image[image > 0]
+
+        plt.hist(image.flatten(), bins = 100, alpha = 0.5, label = path.split("/")[-1])
+        
+    plt.title("Intensity Histogram Across Subjects")
+    plt.xlabel('Intensity')
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.show()
+
+if __name__ == '__main__':
+    image_paths = [r"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MISSSSSA\Final_project\TrainingValidationTestSets\Training_Set\IBSR_01\IBSR_01.nii.gz".replace("\\", "/"),
+                   r"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MISSSSSA\Final_project\TrainingValidationTestSets\Training_Set\IBSR_03\IBSR_03.nii.gz".replace("\\", "/"),
+                   r"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MISSSSSA\Final_project\TrainingValidationTestSets\Training_Set\IBSR_04\IBSR_04.nii.gz".replace("\\", "/"),
+                   r"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MISSSSSA\Final_project\TrainingValidationTestSets\Training_Set\IBSR_05\IBSR_05.nii.gz".replace("\\", "/")]
+    
+    compary_intensity_histograms(image_paths)
