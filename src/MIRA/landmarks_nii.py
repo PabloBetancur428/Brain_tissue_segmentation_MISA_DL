60f03b16b469e413115618bb31baddacffa7dd09
PabloBetancur28 <pablo1326699@gmail.com>
2024-12-07 01:56:44 +0100

    Properly got landmarks wrt image

--- src/MIRA/landmarks_nii.py
+++ src/MIRA/landmarks_nii.py
@@ -1,5 +1,6 @@
 import numpy as np
 import SimpleITK as sitk
+import nibabel as nib
 
 
 def load_landmarks_with_tabs(file_path):
@@ -12,6 +13,7 @@ def load_landmarks_with_tabs(file_path):
             numeric_parts = [float(part) for part in parts[:3] if part.strip()]
             # Append to landmarks if we have all three values (x, y, z)
             if len(numeric_parts) == 3:
+                print(numeric_parts)
                 landmarks.append(numeric_parts)
             else:
                 print(f"Skipping invalid or incomplete line: {line.strip()}")
@@ -33,7 +35,10 @@ def create_nifti(landmarks, reference_image_path, output_nifti_path):
     #Draw points from the coordinates of the real image
     for coord in landmarks:
         index = reference_image.TransformPhysicalPointToIndex(coord.tolist())
+        #print(f"Coordinate: {coord}, Index: {index}, Image Size: {image_size}")
+        #print("a")
         if all(0 <= idx < sz for idx, sz in zip(index, image_size)):
+            print("f")
             landmark_image[index] = 1  # Mark the voxel
 
     #Save
@@ -45,7 +50,25 @@ if __name__ == "__main__":
 
     landmark_txt_file = fr"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MIRRRRA\Final_project\Training data-20241123\copd3\copd3\copd3_300_eBH_xyz_r1.txt".replace("\\", "/")
     reference_image_path = fr"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MIRRRRA\Final_project\Training data-20241123\copd3\copd3\copd3_eBHCT.nii.gz".replace("\\", "/")
-    output_nifti_path = fr"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MIRRRRA\Final_project\Training data-20241123\copd3\copd3\copd3_300_eBH_xyz_r1.nii".replace("\\", "/")
+    output_nifti_path = fr"C:\Users\User\Desktop\UDG_old_pc\UDG\Subjects\MIRRRRA\Final_project\Training data-20241123\copd3\copd3\3333copd3_300_eBH_xyz_r1.nii".replace("\\", "/")
 
-    landmarks = load_landmarks_with_tabs(landmark_txt_file)
-    create_landmark_nifti = create_nifti(landmarks, reference_image_path, output_nifti_path)
+    ref_img = nib.load(reference_image_path)
+    ref_data = ref_img.get_fdata()
+    ref_affine = ref_img.affine
+    ref_shape = ref_data.shape
+
+    landmark_volume = np.zeros(ref_shape, dtype = np.int16)
+    with open(landmark_txt_file, "r") as f:
+        for line in f:
+            parts = line.strip().split("\t")
+            x, y, z = [int(part.split(".")[0]) - 1 for part in parts[:3] if part.strip()] #♠ we have to substract one bc of different origin between image and landmarks
+
+
+            if (0<= x < ref_shape[0] and (0<= y < ref_shape[1]) and (0<= z < ref_shape[2])):
+                landmark_volume[x, y, z] = 1
+            
+            else:
+                print(f"Landmark {x}, {y}, {z} is out of image bounds")
+    
+    landmark_img = nib.Nifti1Image(landmark_volume, ref_affine)
+    nib.save(landmark_img, output_nifti_path)
